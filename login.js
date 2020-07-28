8ae47df277b89c8739806c3a83a2050729daf173
Ethan N. Letrong <letronge@csu.fullerton.edu>
2020-07-27 20:35:38 -0700

    Uploaded Login.js
    This has a basic script to deal with the user's login information. May need to alter the file location when a login is successful though.

---
+++ login.js
@@ -0,0 +1,32 @@
+
+
+// We will give the user several attemps. This way we can create a loop
+var attempt = 3;
+
+//Button Click Functions
+function verify(){
+  var username = document.getElementById('uName').value;
+  var password = document.getElementById('pWord').value;
+
+  //Assuming that the login is correct, we forward them to the admin page
+  if (username == "Admin" && password == "Pass123"){
+    alert ("Login Attempt is successful");
+    window.location = "AdminControl.html";
+  }
+
+  //This is FAILED ATTEMPT CASES:
+  else {
+    attempt --;
+    alert("Attempt Failed. You have "+attempt+" tries left.");
+
+    //If the user's used all attempts, we lock them out.
+    //They can get more attempts when they refresh the page
+    if( attempt == 0){
+      document.getElementById("uName").disabled = true;
+      document.getElementById("pWord").disabled = true;
+      document.getElementById("submit").disabled = true;
+      return false;
+    }
+  }
+
+}
